f9ae6e28bc7caf02bf502be80a69072f3408917e
FreekAlberti <federicoalberi96@gmail.com>
2020-09-21 13:03:33 +0200

    codice migliorato

--- js/script.js
+++ js/script.js
@@ -7,57 +7,30 @@
 
 $(document).ready(function() {
 
-  var dataInizio = "2018-01-01";
+  var dataInizio = moment("2018-12-01");
   var giorniMese = moment(dataInizio).daysInMonth();
-
-  // handlebars1
-
-  var source1 = $("#entry-template1").html();
-  var template1 = Handlebars.compile(source1);
-
-  var context1 = {
-    "mese" : moment(dataInizio).format("MMMM")
-  };
-
-  var html1 = template1(context1);
-
-  $("#headerCalendario").append(html1);
-
-  // handlebars2
-
-  var source2 = $("#entry-template2").html();
-  var template2 = Handlebars.compile(source2);
-
-  for (var i = 1; i <= giorniMese; i++) {
-    var giorno = i;
-    if (giorno <= 9) {
-      giorno = "0" + giorno;
-    }
-    var data = moment(dataInizio).format("YYYY") + "-" + moment(dataInizio).format("MM") + "-" + giorno;
-    var context2 = {
-      "giorno" : giorno,
-      "mese" : moment(dataInizio).format("MMMM"),
-      "data" : data
-    };
-
-    var html2 = template2(context2);
-
-    $("#calendario").append(html2);
-  }
+  var month = dataInizio.format("M") - 1;
 
   $.ajax(
     {
-      url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
-      method: "GET",
-      success: function (data, stato) {
+      "url": "https://flynn.boolean.careers/exercises/api/holidays",
+      "data": {
+        "year": 2018,
+        "month": month
+      },
+      "method": "GET",
+      "success": function (data, stato) {
+        printCalendar(dataInizio, giorniMese);
         printHoliday(data.response);
       },
-      error: function (richiesta, stato, errori) {
+      "error": function (richiesta, stato, errori) {
         console.log(errori);
       }
     }
   );
 });
+
+// FUNCTION
 
 function printHoliday(holiday) {
   for (var i = 0; i < holiday.length; i++) {
@@ -65,5 +38,25 @@ function printHoliday(holiday) {
     var festivitaNome = holiday[i].name;
     $(".day[data-data=" + festivitaData + "]").addClass("holiday");
     $(".day[data-data=" + festivitaData + "] .holidayStyle").text(" - " + festivitaNome);
+  }
+}
+
+function printCalendar(dataInizio, giorniMese) {
+  $("h1").text(dataInizio.format("MMMM YYYY"));
+  var source = $("#entry-template").html();
+  var template = Handlebars.compile(source);
+  for (var i = 1; i <= giorniMese; i++) {
+    var giorno = i;
+    if (giorno <= 9) {
+      giorno = "0" + giorno;
+    }
+    var data = moment(dataInizio).format("YYYY") + "-" + moment(dataInizio).format("MM") + "-" + giorno;
+    var context = {
+      "giorno" : giorno,
+      "mese" : moment(dataInizio).format("MMMM"),
+      "data" : data
+    };
+    var html = template(context);
+    $("#calendario").append(html);
   }
 }
